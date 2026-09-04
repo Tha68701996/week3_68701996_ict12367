76845c8ea9bacded910582c0ea4c77f0a6252681
Saga <tnd.mme@gmail.com>
2026-09-04 12:09:32 +0700

    Initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/customer',
+    name: 'customer',
+    component: () => import('../views/Customer.vue')
+  },
+  {
+    path: '/employee',
+    name: 'employee',
+    component: () => import('../views/Employee.vue')
   }
 ]
 
